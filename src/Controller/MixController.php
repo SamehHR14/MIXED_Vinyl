5f042a8284ba91bc484c19117fd8403308ab70f1
Sameh Horri <horrisamah2000@gmail.com>
2023-11-22 09:20:06 +0100

    sameh::Creation of MixController class

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,14 +2,16 @@
 <?php
 
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\VinylMix;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MixController extends AbstractController
 {
     #[Route('/mix/new')]
-    public function new(): Response
+    public function new(EntityManagerInterface $entityManager): Response
     {
         $mix = new VinylMix();
         $mix->setTitle('Do you Remember... Phil Collins?!');
@@ -17,6 +19,15 @@ class MixController extends AbstractController
         $mix->setGenre('pop');
         $mix->setTrackCount(rand(5, 20));
         $mix->setVotes(rand(-50, 50));
-        dd($mix);
+        $entityManager->persist($mix);
+        $entityManager->flush();
+         
+         
+        return new Response(sprintf(
+            'Mix %d is %d tracks of pure 80\'s heaven',
+            $mix->getId(),
+            $mix->getTrackCount()
+        ));
     }
 }
+?>
